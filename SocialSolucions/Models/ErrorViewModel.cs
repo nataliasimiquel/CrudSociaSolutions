6d8d331ae90a9ae57469cda2e8f5c1b6c976571c
Natália Simiquel <nsimiquel@gmail.com>
2022-04-15 09:44:47 -0300

    SocialSolutions

---
+++ SocialSolucions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SocialSolucions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
