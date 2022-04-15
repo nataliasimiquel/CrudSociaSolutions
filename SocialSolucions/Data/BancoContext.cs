c6d39c754dddac3f943b5e18c69eac7ee84eabca
Natália Simiquel <nsimiquel@gmail.com>
2022-04-15 10:33:14 -0300

    15-04

---
+++ SocialSolucions/Data/BancoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialSolucions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialSolucions.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> Options) : base(Options)
+        {
+        }
+
+    public DbSet<ClienteModel> Cliente { get; set; }
+    }
+}
---
+++ SocialSolucions/Models/ClienteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialSolucions.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+
+    }
+}
